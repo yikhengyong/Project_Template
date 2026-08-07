53900b8b85a88e00fa4e6b37b92a91e1545967ac
Yong Yik Heng (Lawrence) <yikhengyong32@gmail.com>
2026-08-07 10:58:13 +0800

    Add main function with variable display and function calls
    Implement main function to display variable values and call custom functions.

---
+++ main.c
@@ -0,0 +1,53 @@
+/**
+* @file     main.c
+* @brief    Print out result
+* @return   None
+*/
+
+#include <stdio.h>
+#include "function.h" 
+
+void function();
+
+int main(void) {
+
+    // Step 1: Display default value of global variable i
+    printf("i default value:%d\n", i);
+
+    // Step 2: Test custom function:compare_string 
+    unsigned int compare_result = compare_strings("string1", "string2");
+    compare_result == 0 ? printf("Strings are equal\n") : printf("Strings are different\n");
+
+    // Step 3: Functions in main.c
+    function();
+
+    // Step 4: Function in header file
+    custom_function();
+
+    // Step 5: Update Global Variable i
+    i = 2;
+
+    printf("i value in main():%d\n", i);
+
+    // Cleanup
+    printf("Error Codes:%d\n", errorCode);
+
+    errorCode = 0;
+
+    return 0;
+}
+
+
+/**
+* @brief  Display i value in function which in main.c
+* @return None
+*/
+
+void function() {
+
+    printf("Hello, World!\n");
+    i = -99999;
+    printf("i value in funtion():%d\n", i);
+
+}
+
